2e8bbc641b4084be059dc01be539d718ba32e634
Talha Rasheed <rajatalha175@gmail.com>
2025-10-20 23:44:19 -0700

    mvvm practice new

---
+++ MVVMPractice/MVVMPractice/Views/AnimationDemo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace MVVMPractice
+{
+    public partial class AnimationDemo : Window
+    {
+        public AnimationDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation moveY = new DoubleAnimation
+            {
+                From = 0,
+                To = 150,
+                Duration = new Duration(TimeSpan.FromSeconds(2)),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            translateTransform.BeginAnimation(System.Windows.Media.TranslateTransform.YProperty, moveY);
+        }
+    }
+}
---
+++ MVVMPractice/MVVMPractice/Views/Nav_Delegation.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace MVVMPractice
+{
+    public partial class Nav_Delegation : Window
+    {
+        public delegate void Notify(string message);
+        public event Notify OnButtonClicked;
+
+        public Nav_Delegation()
+        {
+            InitializeComponent();
+            OnButtonClicked += ShowMessage;
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            OnButtonClicked?.Invoke("Button was clicked! Event triggered successfully.");
+        }
+
+        private void ShowMessage(string message)
+        {
+            txtMessage.Text = message;
+        }
+    }
+}
